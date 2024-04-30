f3aeaf13f6980c1b83420d4b192b45d07c12906a
xye <xye@xye>
2024-04-30 04:13:30 +0100

    Implemented ORL Event

---
+++ VRChatAPI/Events/OnRoomLeft.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VRChatAPI.Events
+{
+    public class OnRoomLeft
+    {
+        public string Data { get; set; } = string.Empty;
+        public DateTime DateTime { get; set; } = DateTime.Now;
+
+        public event EventHandler<OnRoomLeft> Event;
+
+        public void ProcessLog(string input)
+        {
+            if (input.Contains("Successfully left room"))
+            {
+                Data = input;
+
+                Event?.Invoke(this, this);
+            }
+        }
+    }
+}
