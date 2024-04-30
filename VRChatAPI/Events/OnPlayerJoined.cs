732d8f773bca6d84f099609f96aa4f0f18be4ace
xye <xye@xye>
2024-04-30 04:09:48 +0100

    Implemented OPJ Event

---
+++ VRChatAPI/Events/OnPlayerJoined.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace VRChatAPI.Events
+{
+    public class OnPlayerJoined
+    {
+        public string Data { get; set; } = string.Empty;
+        public DateTime DateTime { get; set; } = DateTime.Now;
+        public string DisplayName { get; set; } = string.Empty;
+
+        public event EventHandler<OnPlayerJoined> Event;
+
+        public void ProcessLog(string input)
+        {
+            var match = Regex.Match(input, @"OnPlayerJoined (.+)");
+            if (match.Success)
+            {
+                DisplayName = match.Groups[1].Value;
+                Event?.Invoke(this, this);
+            }
+        }
+    }
+}
