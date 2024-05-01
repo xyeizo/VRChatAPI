7e3f37c24ec14ae15a819128fc3aefa7af2a3ae0
xye <xye@xye>
2024-05-01 17:50:03 +0100

    Added IsValid() to OnRoomJoined

--- VRChatAPI/Events/OnRoomJoined.cs
+++ VRChatAPI/Events/OnRoomJoined.cs
@@ -34,5 +34,13 @@ namespace VRChatAPI.Events
             }
             return null;
         }
+
+        public bool IsValid()
+        {
+            if (!string.IsNullOrEmpty(WorldId) && !string.IsNullOrEmpty(RoomInstance))
+                return true;
+
+            return false;
+        }
     }
 }
